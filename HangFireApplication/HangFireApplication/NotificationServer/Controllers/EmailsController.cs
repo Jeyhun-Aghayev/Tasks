29b033807f71148fc343e04283093b49bb0c6155
Jeyhun S. Aghayev <ceyhunsa@code.local>
2024-08-19 13:48:09 +0400

    asd

---
+++ HangFireApplication/HangFireApplication/NotificationServer/Controllers/EmailsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotificationServer.Models;
+using NotificationServer.Services;
+
+namespace NotificationServer.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class EmailsController : ControllerBase
+{
+    private readonly IMailService _mailService;
+
+    public EmailsController(IMailService mailService)
+    {
+        _mailService = mailService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMail([FromBody] EmailBody request)
+    {
+        try
+        {
+            await _mailService.SendEmailAsync(request);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+
+            return BadRequest(ex.Message);
+        }
+    }
+
+}
---
+++ HangFireApplication/HangFireApplication/NotificationServer/Services/IMailService.cs
@@ -0,0 +1,67 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+using NotificationServer.Configurations;
+using NotificationServer.Models;
+
+
+namespace NotificationServer.Services
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(EmailBody email);
+    }
+    public class MailService : IMailService
+    {
+        private readonly IConfiguration _configuration;
+
+        public MailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(EmailBody email)
+        {
+            var configuration = _configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(configuration.DisplayName, configuration.From));
+            emailMessage.To.Add(new MailboxAddress("Kemalll", email.To));
+
+            if (!string.IsNullOrWhiteSpace(email.Cc))
+            {
+                emailMessage.Cc.Add(new MailboxAddress("Ceyhun", email.Cc));
+            }
+            if (!string.IsNullOrWhiteSpace(email.Bcc))
+            {
+                emailMessage.Bcc.Add(new MailboxAddress("hesen", email.Cc));
+            }
+            emailMessage.Subject = email.Subject;
+            var bodyBuilder = new BodyBuilder
+            {
+                HtmlBody = email.Body
+            };
+
+            if (email.Attachments.Count > 0)
+            {
+                foreach (var attachment in email.Attachments)
+                {
+                    bodyBuilder.Attachments.Add(attachment.FileName, attachment.FileContent);
+                }
+            }
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+            using (var client = new SmtpClient())
+            {
+                try
+                {
+                    await client.ConnectAsync(configuration.SmtpServer, configuration.Port, false);
+                    await client.AuthenticateAsync(configuration.UserName, configuration.Password);
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
